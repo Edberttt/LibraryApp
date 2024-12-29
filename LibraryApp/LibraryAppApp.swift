3777335d18e3c5e72ad57c714f867a23f348b72c
Edberttt <115908360+Edberttt@users.noreply.github.com>
2024-12-29 12:17:21 +0700

    Initial Commit

---
+++ LibraryApp/LibraryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryAppApp.swift
+//  LibraryApp
+//
+//  Created by Edbert Chandradinata on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LibraryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LibraryAppTests/LibraryAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryAppTests.swift
+//  LibraryAppTests
+//
+//  Created by Edbert Chandradinata on 29/12/24.
+//
+
+import Testing
+@testable import LibraryApp
+
+struct LibraryAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
